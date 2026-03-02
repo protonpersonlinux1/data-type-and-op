7e18d422f3cca25344d092e10a75a855c7a345ff
Dev? <ayanthakur.mumensom@outlook.com>
2026-03-02 16:53:50 +0100

    Add files via upload

---
+++ leapyear.java
@@ -0,0 +1,22 @@
+public class leapyear {
+
+    public static void main(String[] args) {
+        int year = 2016;
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a leap year.");
+        } else {
+            System.out.println(year + " is not a leap year.");
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                return year % 400 == 0;
+            }
+            return true;
+        }
+        return false;
+    }
+}
---
+++ math.java
@@ -0,0 +1,19 @@
+class math{
+        public static void main(String[] args) {
+        
+        int a = 10;
+        int b = 5;
+
+        int sum = a + b;
+        int diff = a - b;
+        int mult = a * b;
+        int div = a / b;
+
+
+                System.out.println("Addition of a&b: " + sum);
+                System.out.println("Subtraction of a&b: " + diff);
+                System.out.println("Multiplication of a&b: " + mult);
+                System.out.println("Division of a&b: " + div);
+
+        }
+    }
