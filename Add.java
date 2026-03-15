1612c080dcaf00b1ac970285169fc2402a8bdcc3
Dev? <ayanthakur.mumensom@outlook.com>
2026-03-15 20:55:48 +0100

    Add files via upload

---
+++ Add.java
@@ -0,0 +1,8 @@
+public class Add {
+    public static void main(String[] args) {
+        int x = 7;
+        int y = 3;
+        int z = x + y;
+        System.out.println("The sum of " + x + " and " + y + " is: " + z);
+    }
+}
